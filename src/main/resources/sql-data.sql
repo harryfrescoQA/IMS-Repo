042bd0ece671241cc3b54fac77b9109ba8f23611
harryfresco <harry.fresco@students.plymouth.ac.uk>
2020-11-18 14:59:20 +0000

    HF: Created the SQL statements to create the database and add a few examples

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,14 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Jordan', 'Harrison');
+INSERT INTO `ims`.`items` (`item_title`, `quantity`, `price`) VALUES ('Fender Stratocaster - White', '100', '699.99');
+INSERT INTO `ims`.`orders` (`customer_id`, `order_date`, `total_cost`) VALUES ('1', '2020-11-18', '699.99');
+INSERT INTO `ims`.`orderline` (`order_id`, `item_id`, `quantity_ordered`) VALUES ('1', '1', '1');
+
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Harry', 'Fresco');
+INSERT INTO `ims`.`items` (`item_title`, `quantity`, `price`) VALUES ('Fender Telecaster - Black', '100', '699.99');
+INSERT INTO `ims`.`orders` (`customer_id`, `order_date`, `total_cost`) VALUES ('2', '2020-11-17', '699.99');
+INSERT INTO `ims`.`orderline` (`order_id`, `item_id`, `quantity_ordered`) VALUES ('2', '2', '1');
+
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Fred', 'Freddy');
+INSERT INTO `ims`.`items` (`item_title`, `quantity`, `price`) VALUES ('PRS - Red', '100', '799.99');
+INSERT INTO `ims`.`orders` (`customer_id`, `order_date`, `total_cost`) VALUES ('3', '2020-11-16', '799.99');
+INSERT INTO `ims`.`orderline` (`order_id`, `item_id`, `quantity_ordered`) VALUES ('3', '3', '1');
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -7,3 +7,26 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    `order_date` DATE NOT NULL,
+    `total_cost` dec(7,2) NOT NULL,
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_title` VARCHAR(255) NOT NULL,
+    `quantity` INT NOT NULL,
+    `price` dec(7,2) NOT NULL,
+    PRIMARY KEY (`item_id`)
+
+);
+CREATE TABLE IF NOT EXISTS `ims`.`orderline` (
+    `order_id` INT(11) NOT NULL,
+    `item_id` INT(11) NOT NULL,
+    `quantity_ordered` INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
